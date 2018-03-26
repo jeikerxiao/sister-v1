2c5536b296948d1d8feb6adf815d964a870ba714
jeikerxiao <jeiker@126.com>
2018-03-26 17:34:06 +0800

    add MeFootView
    add MeFootView

--- Sister-v1/Sister-v1/Me/ViewModel/MeFootViewVM.swift
+++ Sister-v1/Sister-v1/Me/ViewModel/MeFootViewVM.swift
@@ -7,8 +7,29 @@
 //
 
 import UIKit
+import Alamofire
 
 class MeFootViewVM {
     
+    class func loadMeVCData(_ finishedBlock: @escaping (_ dataArray: [MeFootModel])->()){
+        // http://api.budejie.com/api/api_open.php?a=square&c=topic
+        let urlString = "http://api.budejie.com/api/api_open.php"
+        let parameter = ["a":"square","c":"topic",]
+        
+        Alamofire.request(urlString, method: .get, parameters: parameter).responseJSON { (respond) in
+            guard let dic = respond.result.value as? [String: Any] else {
+                return
+            }
+            guard let datas = dic["square_list"] as? [[String: Any]] else {
+                return
+            }
+            var meFootViewModels = [MeFootModel]()
+            for dic in datas {
+                let model = MeFootModel(dic)
+                meFootViewModels.append(model)
+            }
+            finishedBlock(meFootViewModels)
+        }
+    }
 
 }
